74a3a762ec03538f3a67b5157d119f109d8505a7
Matthew Tempinski <matt.tempinski@gmail.com>
2014-03-14 11:56:37 -0500

    Page Counter fix
    Homepage counter wasn't updating

--- features/default/views/index.php
+++ features/default/views/index.php
@@ -1,6 +1,12 @@
 <?php
 
-// Shhh.
 $HomePage = new HomePage();
 $i = $HomePage->redirect();
+
+$q = $tData->query("SELECT * FROM `".$tDataClass->prefix."_pages` WHERE `alias`='".$i['alias']."'");
+$row = $q->fetch_assoc();
+
+$views = $row['views'] + 1;
+$tData->query("UPDATE `".$tDataClass->prefix."_pages` SET `views`='".$views."' WHERE `alias`='".$i['alias']."'");
+
 echo $HomePage->page_content;
