7f9dca3818798add6c13aedc4972543e3300c862
Matthew Tempinski <matt.tempinski@gmail.com>
2014-03-14 12:28:23 -0500

    Added comments

--- system/tTheme.class.php
+++ system/tTheme.class.php
@@ -1,14 +1,119 @@
 <?php
 
+/**
+ * tTheme - Theamus theme parsing class
+ * PHP Version 5.5.3
+ * Version 1.0
+ * @package Theamus
+ * @link http://www.theamus.com/
+ * @author Matthew Tempinski (Eyraahh) <matt.tempinski@theamus.com>
+ * @copyright 2014 Matthew Tempinski
+ */
+
 class tTheme {
+    /**
+     * Holds all of the information relative to the user
+     *
+     * @var object $tUser
+     */
+    private $tUser;
+
+
+    /**
+     * Class variable to handle data
+     *
+     * @var object $tDataClass
+     */
+    private $tDataClass;
+
+
+    /**
+     * Mysqli object
+     *
+     * @var object $tData
+     */
+    private $tData;
+
+
+    /**
+     * Holds the data given by the call
+     *
+     * @var array $data
+     */
     protected $data;
+
+
+    /**
+     * Holds the data found in the theme's configuration file
+     *
+     * @var array $config
+     */
     protected $config;
+
+
+    /**
+     * Holds the data that is accessible to the theme
+     *
+     * @var array $nice_data
+     */
+    private $nice_data;
+
+
+    /**
+     * The template file found in the theme's configuration settings
+     *
+     * @var string $templage
+     */
     private $template;
+
+
+    /**
+     * The contents of the HTML markup in the header tags (<head></head>)
+     *
+     * @var string $header
+     */
     private $header;
+
+
+    /**
+     * True/False on whether or not the admin panel is accessible
+     *
+     * @var boolean $admin
+     */
+    private $admin;
+
+
+    /**
+     * The contents of the body HTML markup before the content
+     *
+     * @var string body
+     */
     private $body;
+
+
+    /**
+     * The contents of the footer HTML markup after the content
+     *
+     * @var string $footer
+     */
     private $footer;
+
+
+    /**
+     * Holds the value of whether or not this output has extra navigation associated
+     *  with it and needs to include the extra navigation class
+     *
+     * @var array $nav
+     */
     private $nav;
 
+
+    /**
+     * Starts and initializes variables to be used by the class.
+     * Then performs a try/catch statement to check/run the theme parsing
+     *
+     * @param array $data
+     */
     public function __construct($data) {
         $this->data = $data;
         $this->tUser = new tUser();
@@ -28,30 +133,62 @@ class tTheme {
         }
     }
 
+
+    /**
+     * Output the page header
+     */
     public function print_header() {
         echo $this->header;
     }
 
+
+    /**
+     * Output the page body
+     */
     public function print_body() {
         echo $this->body;
     }
 
+
+    /**
+     * Output the page footer
+     */
     public function print_footer() {
         echo $this->footer;
     }
 
+
+    /**
+     * Gets the theme folder from the data given
+     *
+     * @return string
+     */
     private function get_theme_folder() {
         $split = strpos($this->data['theme'], "\\") !== false ? "\\" : "/";
         $path = explode($split, trim($this->data['theme'], $split));
         return array_pop($path);
     }
-    
+
+
+    /**
+     * Gets the platform settings from the database
+     *
+     * @return array
+     * @throws Exception
+     */
     private function get_settings() {
         $q = $this->tData->query("SELECT * FROM `".$this->tDataClass->prefix."_settings`");
         if (!$q) throw new Exception("Error getting system settings information from the database.");
         return $q->fetch_assoc();
     }
 
+
+    /**
+     * Cleans the incoming data from the call and sanitizes it to be called from
+     *  the theme
+     *
+     * @return array
+     */
     private function clean_data() {
         $settings = $this->get_settings();
         $ret['title'] = isset($this->data['title']) ? $this->data['title']." - ".$this->data['name'] : $this->data['name'];
@@ -63,6 +200,13 @@ class tTheme {
         return $ret;
     }
 
+
+    /**
+     * Gets information from the theme's configuration file
+     *
+     * @return array
+     * @throws Exception
+     */
     private function get_config() {
         $config_path = path($this->data['theme']."/config.json");
         if (file_exists($config_path)) {
@@ -71,6 +215,13 @@ class tTheme {
         throw new Exception("Cannot locate the theme configuration file.");
     }
 
+
+    /**
+     * Gets the template file requested based on the configuration file and the call settings
+     *
+     * @return string $ret
+     * @throws Exception
+     */
     private function get_template() {
         for ($i = 0; $i < count($this->config->layouts); $i++) {
             if ($this->config->layouts[$i]->layout == "default") $ret = $this->config->layouts[$i]->file;
@@ -80,6 +231,13 @@ class tTheme {
         throw new Exception("Cannot find the template file in the theme configuration.");
     }
 
+
+    /**
+     * Gets the contents of the template index file
+     *
+     * @return string
+     * @throws Exception
+     */
     private function get_template_contents() {
         $template_path = path($this->data['theme']."/".$this->template);
         if (file_exists($template_path)) {
@@ -88,6 +246,13 @@ class tTheme {
         throw new Exception("Cannot find the template file in the directory structure.");
     }
 
+
+    /**
+     * Parses all of the template file to include everything required and requested
+     *
+     * @param string $contents
+     * @return string $ret
+     */
     private function parse_template_contents($contents = "") {
         $this->get_areas($contents);
         $ret = $this->set_variables($contents);
@@ -101,6 +266,14 @@ class tTheme {
         return $ret;
     }
 
+
+    /**
+     * Finds any calls for variables in the theme contents and replaces them with actual
+     *  data/values
+     *
+     * @param string $contents
+     * @return string $output
+     */
     private function set_variables($contents) {
         $output = preg_replace_callback(
             array("/{t:var='(.*?)':}/s", "/{t:var=\"(.*?)\":}/s"),
@@ -110,6 +283,14 @@ class tTheme {
         return $output;
     }
 
+
+    /**
+     * Finds any calls for loops in the theme contents and executes them
+     *
+     * @param string $contents
+     * @return string $output
+     * @throws Exception
+     */
     private function do_loops($contents) {
         $output = preg_replace_callback(
             "/{t:loop\((.*?)\):}(.*?){t:\/loop:}/s",
@@ -128,6 +309,14 @@ class tTheme {
         return $output;
     }
 
+
+    /**
+     * In a loop, finds any variables being requested and replaces them with actual data
+     *
+     * @param string $template
+     * @param array $clean_vars
+     * @return string
+     */
     private function set_loop_variables($template, $clean_vars) {
         $return_template = "";
         foreach ($clean_vars as $cv) {
@@ -141,6 +330,13 @@ class tTheme {
         return $return_template;
     }
 
+
+    /**
+     * Cleans variables for a theme
+     *
+     * @param array $vars
+     * @return array
+     */
     private function clean_theme_vars($vars) {
         $ret = [];
         foreach ($vars as $v) {
@@ -150,6 +346,13 @@ class tTheme {
         return $ret;
     }
 
+
+    /**
+     * Adds the platform header data to the <head></head> tags
+     *
+     * @param string $contents
+     * @return string $output
+     */
     private function add_header_data($contents) {
         $output = preg_replace_callback(
             "/<head>(.*?)<\/head>/s",
@@ -163,6 +366,13 @@ class tTheme {
         return $output;
     }
 
+
+    /**
+     * Finds all of the areas in the theme contents, mostly looking for navigation areas
+     *
+     * @param string $contents
+     * @return
+     */
     private function get_areas($contents) {
         $output = preg_replace_callback(
             array("/{t:area=\"(.*?)\":}/s", "/{t:area='(.*?)':}/s"),
@@ -179,6 +389,14 @@ class tTheme {
         } else $this->nice_data['nav'] = "";
     }
 
+
+    /**
+     * Finds all of the requested areas in the theme's file contents and replaces them with
+     *  their respective area files
+     *
+     * @param string $contents
+     * @return string $output
+     */
     private function set_areas($contents) {
         $output = preg_replace_callback(
             array("/{t:area=\"(.*?)\":}/s", "/{t:area='(.*?)':}/s"),
@@ -188,6 +406,14 @@ class tTheme {
         return $output;
     }
 
+
+    /**
+     * Finds the area file from the configuration settings and returns the contents of the file
+     *  to be used
+     *
+     * @param string $area
+     * @return string
+     */
     private function get_area_content($area) {
         if ($area == "extra-nav" && (!isset($this->data['nav']) || $this->data['nav'] == "")) return "";
         if (property_exists($this->config->areas, $area)) {
@@ -197,6 +423,13 @@ class tTheme {
         return "";
     }
 
+
+    /**
+     * Finds all navigation areas and replaces them with the respective navigation
+     *
+     * @param string $contents
+     * @return string $output
+     */
     private function get_navigation($contents) {
         $output = preg_replace_callback(
             array("/{t:nav='(.*?)':}/s", "/{t:nav=\"(.*?)\":}/s"),
@@ -215,6 +448,13 @@ class tTheme {
         return $output;
     }
 
+
+    /**
+     * Defines all of the user areas that can be used in a theme
+     *
+     * @param string $contents
+     * @return string $output
+     */
     private function set_user_areas($contents) {
         $ret = $contents;
         $types = array("admin", "noadmin", "useradmin", "user", "nouser");
@@ -222,6 +462,15 @@ class tTheme {
         return $ret;
     }
 
+
+    /**
+     * Loops through all of the possible types of user areas and performs the relative
+     *  check in order to show content or not.
+     *
+     * @param string $contents
+     * @param string $type
+     * @return string $output
+     */
     private function set_user_area($contents, $type) {
         switch ($type) {
             case "admin":
@@ -256,6 +505,14 @@ class tTheme {
         return $output;
     }
 
+
+    /**
+     * Finds any requests to get the currently logged in user's information and replaces it
+     *  with actual data
+     *
+     * @param string $contents
+     * @return string
+     */
     private function set_user_variables($contents) {
         $output = preg_replace_callback(
             array("/{t:user_var='(.*?)':}/s", "/{t:user_var=\"(.*?)\":}/s"),
@@ -267,6 +524,12 @@ class tTheme {
         return $output;
     }
 
+
+    /**
+     * Defines the parts of the page that are able to be shown
+     *
+     * @param string $contents
+     */
     private function define_parts($contents) {
         if (strpos($contents, "{t:admin_panel:}") !== false) {
             $this->admin = true;
