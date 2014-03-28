9ae78f0e6c437e216b51263d5b0de57277ad5a53
Matthew Tempinski <matt.tempinski@gmail.com>
2014-03-28 16:39:47 -0500

    Updated the home app update checker

--- features/default/home-apps/updatecheck/main.php
+++ features/default/home-apps/updatecheck/main.php
@@ -1,32 +1,45 @@
 <?php
 
-// Get the system settings
-$settingsTable = $tDataClass->prefix . '_settings';
-$sql['settings'] = 'SELECT * FROM `' . $settingsTable . '`';
-$qry['settings'] = $tData -> query($sql['settings']);
-$settings = $qry['settings'] -> fetch_assoc();
+$error = false; // Start out the day nicely.
 
-// Get the update server
-$server = "http://theamus.com/releases/update/";
-$info = @file_get_contents($server);
-
-if (!$info) {
-    echo 'There was an issue connecting to the update server.';
-    run_after_ajax('backToCheck');
+// Define and check the system settings
+$settings_query = $tData->query("SELECT * FROM `".$tDataClass->prefix."_settings`");
+if (!$settings_query) {
+    $error = "There was an error gathering information about the host system.";
 } else {
-    // Get the update information
-    $info = json_decode($info, true);
+    $settings = $settings_query->fetch_assoc();
+}
 
-    if (!empty($info) && in_array($settings['version'], $info['old_versions'])):
-?>
-        <?=notify("admin", "success", "There's an update available for your system!
-        <a href='#' onclick=\"return admin_go('settings', 'settings/settings/');\">Update Now!</a>")?>
+// Make the AJAX call to the update server
+//$server = "http://theamus.com/releases/update/";
+$server = "http://localhost/theamus/release-manager/update-info/";
+$info = $tDataClass->api(array(
+    "type"  => "get",
+    "url"   => $server,
+    "method"=> array("Releases", "get_update_info"),
+    "key"   => "dQPlembXjBfGvmCqH0Cot9uMeKAbRkTdr6ysWK1V50U="
+));
+
+// Check the results
+if ($info['error']['status'] == 1) {
+    $error = "There was an issue retrieving the data from the server";
+} else {
+    $update_data = $info['response']['data'];
+}
+
+if ($error != false) {
+    echo $error;
+} else {
+    if (in_array($settings['version'], $update_data['old_versions'])) {
+        notify("admin", "success", "There's an update available for your system!".
+            " - <a href='#' onclick=\"return admin_go('settings', 'settings/settings/');\">Update Now!</a>");
+    ?>
         <div style="border: 1px solid #EEE; margin: 10px 0; padding: 5px 5px 10px;
              overflow-y: auto; max-height: 150px;">
-            <?=$info['notes']?>
+            <?=$update_data['notes']?>
         </div>
-<?php
-    else:
+    <?php
+    } else {
         echo 'There are no updates available at this time for your system.';
-    endif;
+    }
 }
