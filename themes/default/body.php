99a4d5141b4b1f8b0e7b864bdfc3e4d01e4c5b1d
Eyrah Temet <matt.tempinski@gmail.com>
2014-04-01 19:06:50 -0500

    Style updates

--- themes/default/body.php
+++ themes/default/body.php
@@ -1,6 +1,6 @@
 <?php $col = ($tTheme->get_page_navigation("extra") != "") ? "col-9" : ""; ?>
 <div class="site_content-header <?=$col?>"><?php echo $tTheme->get_page_variable("header"); ?></div>
-<div class="site_content-full <?=$col?>">
+<div class="site_content-full site_content <?=$col?>">
     <?php $tTheme->content(); ?>
 </div>
 <div class="clearfix"></div>
